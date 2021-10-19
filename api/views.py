67b54afbe888327df4fdd50e7d5f2e059eeed384
akayjoshi <ajaypareek801@gmail.com>
2021-10-19 12:44:42 +0530

    django serialization with Serializer class

--- api/views.py
+++ api/views.py
@@ -1,1 +1,20 @@
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import JsonResponse, HttpResponse
+
+
+
+# Create your views here.
+def stu_detail(request, id):
+    stu = Student.objects.get(id=id) # complex data
+    serializer = StudentSerializer(stu) # serialized data mean in python native dict in side .data attribute
+    json_data = JSONRenderer().render(serializer.data) # convert data to JSON data
+    return HttpResponse(json_data, content_type='application/json')
+
+def stu_list(request):
+    stu = Student.objects.all() # complex data
+    serilizer = StudentSerializer(stu, many=True) # make list of serialized data
+    # json_data = JSONRenderer().render(serilizer.data)
+    return JsonResponse(serilizer.data, safe=False)
--- ews1/urls.py
+++ ews1/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('stu/<int:id>', views.stu_detail),
+    path('stu/', views.stu_list),
 ]
